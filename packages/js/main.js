b6854141c77793bc552ef5b987da01f95a758793
Libin Raj <137781659+libin-raj@users.noreply.github.com>
2024-06-05 10:26:57 +0000

    65

--- packages/js/main.js
+++ packages/js/main.js
@@ -251,3 +251,18 @@ if (!window.matchMedia("(max-width: 1500px)").matches) {
         stagger: 0.25
     });
 }
+
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2000,
+  delay: 200,
+//     reset: true
+});
+
+sr.reveal(', header, #home, .skills__text',{delay: 200}); 
+sr.reveal('nav container, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200});
